c55da23294d2113a3421e7d9dfdee7f54ce902f4
Sarah Abouelela <abouelelas@gmail.com>
2020-08-09 12:03:49 -0400

    wrote prompts in js file

--- app.js
+++ app.js
@@ -9,14 +9,171 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
+const teamMembers = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-let createTeam = [];
-createTeam.push(new Manager(inquirer));
-createTeam.push(new Engineer(inquirer));
-createTeam.push(new Intern(inquirer));
+function createTeam() {
+   return inquirer
+        .prompt([
+
+            {
+                type: "list",
+                name: "memberchoice",
+                message: "What type of team member are you?",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern",
+                    "No members to add"
+
+                ]
+            }
+            
+        ]).then(userChoice => {
+            switch (userChoice.memberChoice) {
+                case "Manager":
+                    return addManager();
+                    break;
+                case "Engineer":
+                    return addEngineer();
+                    break;
+                case "Intern":
+                    return addIntern();
+                    break;
+                case "No more employees":
+                    break
+            }
+        })
+    function addManager() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your first name?",
+                    name: "managerName"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your employee ID?",
+                    name: "managerID"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "managerEmail"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your office number?",
+                    name: "managerOfficeNumber"
+                }
+
+            ]).then(userChoice => {
+                console.log(userChoice);
+
+                const manager = new Manager(userChoice.managerName, userChoice.managerID, userChoice.managerEmail, userChoice.managerOfficeNumber)
+
+                teamMembers.push(manager)
+
+               createTeam();
+
+            })
+    }
+    function addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your first name?",
+                    name: "engineerName"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your employee ID?",
+                    name: "engineerID"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "engineerEmail"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your GitHub username?",
+                    name: "gitHubUsername"
+                }
+
+            ]).then(userChoice => {
+                console.log(userChoice);
+
+                const engineer = new Engineer(userChoice.engineerName, userChoice.engineerID, userChoice.engineerEmail, userChoice.gitHubUsername)
+
+                teamMembers.push(engineer)
+
+              createTeam();
+
+            })
+    }
+    function addIntern() {
+       inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your first name?",
+                    name: "internName"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your employee ID?",
+                    name: "internID"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "internEmail"
+                },
+
+                {
+                    type: "input",
+                    message: "What is the name of your school?",
+                    name: "internSchool"
+                }
+
+            ]).then(userChoice => {
+                console.log(userChoice);
+
+                const intern = new Intern(userChoice.internName, userChoice.internID, userChoice.internEmail, userChoice.internSchool)
+
+                teamMembers.push(intern)
+
+               return createTeam();
+
+            })
+    }
+
+}
+
+createTeam()
+    .then(() => {
+        let html = render(teamMembers);
+        fs.writeFile("team.html", html, 'utf8', () => {
+            console.log("Finished")
+        });
+
+    })
+module.exports = teamMembers
+
+// createTeam.push(new Manager(inquirer));
+// createTeam.push(new Engineer(inquirer));
+// createTeam.push(new Intern(inquirer));
 // 3 separate functions for these 3 prompts
 
 
